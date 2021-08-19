821a3213aabf9744b85b4911b5dad6a38be4dbc4
Behnam Safari <safari.b1987@gmail.com>
2021-08-19 19:05:22 +0430

    Climbing the Leaderboard

---
+++ ClimbingtheLeaderboard.py
@@ -0,0 +1,49 @@
+# https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+
+def climbingLeaderboard(ranked, player):
+    ranked = sorted(list(set(ranked)), reverse=True)
+    player = sorted(player, reverse=True)
+    
+    l=len(ranked)
+    j=0
+    
+    ans=[]
+    for i in range(len(player)):
+        while j<l and player[i]<ranked[j]:
+            j+=1
+        
+        ans.append(j+1)
+        
+    return ans[::-1]
+
+
+if __name__ == '__main__':
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    print('\n'.join(map(str, result)))
